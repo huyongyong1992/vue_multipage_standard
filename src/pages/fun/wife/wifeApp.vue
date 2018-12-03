c4de1251be25da9fdcb7cde0085c66ac78122fb3
huyongyong1992 <young.hu@pdabc.com>
2018-12-03 10:52:10 +0800

    update

--- src/pages/fun/wife/wifeApp.vue
+++ src/pages/fun/wife/wifeApp.vue
@@ -3,15 +3,15 @@
   <div class="sign">
     <!-- <x-input placeholder="请输入你的身高" v-model="height"></x-input> -->
     <x-button @click.native="onGenerate" type="warn" text="一键生成" style="margin:20px 0"></x-button>
-    <group >
-      <cell title="身高" :value="obj.height" ></cell>
+    <group v-if="count > 0">
+      <cell title="身高" :value="obj.height"></cell>
       <cell title="体重" :value="obj.weight"></cell>
-      <cell title="发型" :value="obj.hair"></cell>
-      <cell title="属性" :value="obj.attr"></cell>
-      <cell title="性格" :value="obj.character"></cell>
+      <cell title="发型" :value="obj.hair" is-link @click.native="onClick(obj.hair)"></cell>
+      <cell title="属性" :value="obj.attr" is-link @click.native="onClick(obj.attr)"></cell>
+      <cell title="性格" :value="obj.character" is-link @click.native="onClick(obj.character)"></cell>
       <cell title="CUP" :value="obj.CUP"></cell>
-      <cell title="肤色" :value="obj.skin"></cell>
-      <cell title="脸型" :value="obj.face"></cell>
+      <cell title="肤色" :value="obj.skin" is-link  @click.native="onClick(obj.skin)"></cell>
+      <cell title="脸型" :value="obj.face" is-link @click.native="onClick(obj.face)"></cell>
     </group>
   </div>
 </div>
@@ -25,7 +25,8 @@ export default {
   data() {
     return {
       obj: {},
-      height: ''
+      height: '',
+      count: 0
     };
   },
   components: {
@@ -35,14 +36,16 @@ export default {
     this.count = Number(window.localStorage.getItem('count')) || 0
   },
   methods: {
-    onGenerate () {
+    onGenerate() {
       this.count += 1
       window.localStorage.setItem('count', this.count)
       if(this.count % 5 === 1) {
         this.obj = generateObj()
       }
-      
-      
+    },
+    onClick(e) {
+      if(!e) return
+      window.location.href = `https://m.baidu.com/sf/vsearch?pd=image_content&word=女明星+${e}&tn=vsearch&atn=page`
     }
   }
 };
