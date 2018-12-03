672a6fcf2844f93f6ce4388931ba60d85acf3f42
huyongyong1992 <young.hu@pdabc.com>
2018-12-03 15:28:32 +0800

    update

--- src/pages/sign/list/listApp.vue
+++ src/pages/sign/list/listApp.vue
@@ -1,20 +1,34 @@
 <template>
-<div >
-  <div class="sign">
-    <li>多页应用demo</li>
-  </div>
+<div class="sign">
+  <grid :rows="3">
+    <grid-item label="拼图游戏" @click.native="onPinTu">
+      
+    </grid-item>
+    <grid-item label="俄罗斯方块" @click.native="onFangkuai">
+    </grid-item>
+  </grid>
 </div>
 </template>
 
 <script>
+import { Grid, GridItem } from 'vux'
 export default {
   data() {
     return {
     };
   },
-
+  components: {
+    Grid,
+    GridItem
+  },
   created() {},
   methods: {
+    onFangkuai() {
+      window.location.href="http://binaryify.github.io/vue-tetris/?lan=zh"
+    },
+    onPinTu() {
+      window.location.href="../fun/puzzle.html"
+    }
   }
 };
 </script>
