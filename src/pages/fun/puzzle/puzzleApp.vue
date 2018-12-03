a6134f01337507cc9c0480d95bc3463d9c7641f6
huyongyong1992 <young.hu@pdabc.com>
2018-12-03 13:54:05 +0800

    update

--- src/pages/fun/puzzle/puzzleApp.vue
+++ src/pages/fun/puzzle/puzzleApp.vue
@@ -6,9 +6,16 @@
       :autoResize="true"
       :cols="cols"
       :rows="rows"
+      :showNumber="showNumber"
       :src="img" />
 
-    <x-button class="btn" type="primary" @click.native="onChange">换一张</x-button>
+    <div class="btn">
+      <x-button type="warn" mini plain @click.native="onWarn">查看提示</x-button>
+      <x-button :type="active === 'easy' ? 'primary' : ''" mini @click.native="onTypeChange('easy')">简单模式</x-button>
+      <x-button :type="active === 'diff'? 'primary' : ''" mini @click.native="onTypeChange('diff')">困难模式</x-button>
+      <img :src="img"/>
+      <x-button  type="primary" @click.native="onChange">换一张</x-button>
+    </div>
   </div>
 </div>
 </template>
@@ -22,19 +29,35 @@ export default {
     return {
       cols: 3,
       rows: 3,
-      img: require('./images/animal1.jpg')
+      img: require('./images/animal1.jpg'),
+      showNumber: false,
+      active: 'easy'
     };
   },
   components: {
     PuzzleBoard, XButton
   },
-  created() {
+  watch: {
+    active(cur, old) {
+      if(cur === 'easy') {
+        this.cols = this.rows = 3
+      } else if(cur === 'diff') {
+        this.cols = this.rows = 4
+      }
+    }
   },
   methods: {
     onChange() {
       const index = parseInt(Math.random()*8)
       console.log(index)
       this.img = require(`./images/animal${index}.jpg`)
+      this.showNumber = false
+    },
+    onWarn() {
+      this.showNumber = true
+    },
+    onTypeChange(e) {
+      this.active = e
     }
   }
 };
@@ -43,8 +66,13 @@ export default {
 <style lang="less">
 .container{
   .btn{
+    width: 100%;
     position: fixed;
     bottom: 0;
+    img{
+      width: 40%;
+      margin: 10px 30%;
+    }
   }
 }
   
