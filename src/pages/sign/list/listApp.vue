ec04b4379695e235199696eba9aa30d8e8a36846
huyongyong1992 <young.hu@pdabc.com>
2018-12-13 10:52:30 +0800

    update

--- src/pages/sign/list/listApp.vue
+++ src/pages/sign/list/listApp.vue
@@ -1,11 +1,9 @@
 <template>
 <div class="sign">
   <grid :rows="3">
-    <grid-item label="拼图游戏" @click.native="onPinTu">
-      
+    <grid-item :label="item.name" @click.native="onClick(item.url)" v-for="(item, index) in gameList" :key="index">
     </grid-item>
-    <grid-item label="俄罗斯方块" @click.native="onFangkuai">
-    </grid-item>
+    
   </grid>
 </div>
 </template>
@@ -15,6 +13,29 @@ import { Grid, GridItem } from 'vux'
 export default {
   data() {
     return {
+      gameList: [
+        {
+          name: '拼图游戏',
+          url: '../fun/puzzle.html'
+        },
+        {
+          name: '俄罗斯方块',
+          url: 'http://binaryify.github.io/vue-tetris/?lan=zh'
+        },
+        {
+          name: '小李飞刀',
+          url: '../game/stickGame.html'
+        },
+        {
+          name: '水果忍者',
+          url: 'https://www.jqhtml.com/yanshi?demourl=wp-content%2Fuploads%2F2017%2F01%2Ftx%2Ftx201701189%2F&demotitle=HTML5%E7%89%88%E5%88%87%E6%B0%B4%E6%9E%9C%E6%B8%B8%E6%88%8F'
+        },
+        {
+          name: '奔月游戏',
+          url: 'https://www.jqhtml.com/yanshi?demourl=wp-content%2Fuploads%2F2017%2F02%2Ftx%2Ftx20170230%2F&demotitle=HTML5%E5%A5%94%E6%9C%88%E6%B8%B8%E6%88%8F'
+        },
+        
+      ]
     };
   },
   components: {
@@ -23,24 +44,13 @@ export default {
   },
   created() {},
   methods: {
-    onFangkuai() {
-      window.location.href="http://binaryify.github.io/vue-tetris/?lan=zh"
-    },
-    onPinTu() {
-      window.location.href="../fun/puzzle.html"
+    onClick(url) {
+      window.location.href = url
     }
   }
 };
 </script>
 
 <style lang="less">
-body {
-  padding: 0 10px;
-}
-li {
-  padding: 5px 10px !important;
-  font-size: 20px;
-  line-height: 20px;
-  list-style: none;
-}
+
 </style>
